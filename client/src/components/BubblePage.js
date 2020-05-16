7b6a1a3996a83b1e049abd5a7e5e546048e0aeaf
John Oblenda <joandong2@gmail.com>
2020-05-16 12:51:01 -0400

    added delete and edit functions

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../util/axiosWithAuth";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
     const [colorList, setColorList] = useState([]);
+
     // fetch your colors data from the server when the component mounts
     // set that data to the colorList state property
+    useEffect(() => {
+        axiosWithAuth()
+            .get("/colors")
+            .then((res) => {
+                setColorList(res.data);
+            })
+            .catch((err) => {
+                console.log("Err is: ", err);
+            });
+    }, []);
 
     return (
         <>
